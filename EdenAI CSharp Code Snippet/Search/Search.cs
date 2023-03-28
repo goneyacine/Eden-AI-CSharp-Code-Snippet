3dfc7845d473f2040f48f30ffa85d0f8d0ce9108
Yacine Betouche <goneyacine@gmail.com>
2023-03-28 07:53:52 +0200

    add search code snippet

---
+++ EdenAI CSharp Code Snippet/Search/Search.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+public class Search
+    {
+        static void Main(string[] args)
+        {
+        HttpClient httpClient = new HttpClient();
+
+       
+
+        HttpRequestMessage request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri("https://api.edenai.run/v2/text/search"),
+            Headers =
+            {
+               { "accept", "application/json" },
+            },
+            Content = new StringContent("{ \"providers\": \"openai\",\"texts\":[\"text one here , keyword\",\" text two here \"],\"query\" : \"keyword\"}")
+            {
+                Headers =
+                {
+                  ContentType = new MediaTypeHeaderValue("application/json")
+                }
+            }
+        };
+
+
+        //pass your api key here 
+        request.Headers.Add("Authorization", "Bearer YOUR_API_KEY_HERE");
+
+
+        using (HttpResponseMessage response = httpClient.Send(request))
+        {
+            JObject json = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+            Console.WriteLine((json["openai"]["items"]).ToString());
+
+            if (!response.IsSuccessStatusCode)
+                Console.WriteLine(response.Content);
+        }
+
+        Console.ReadLine();
+
+    }
+}
