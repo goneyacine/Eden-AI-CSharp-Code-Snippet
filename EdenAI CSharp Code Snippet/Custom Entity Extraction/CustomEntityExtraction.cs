0310face78501d0ed2775d23faf530f83eabcf01
Yacine Betouche <goneyacine@gmail.com>
2023-03-28 09:03:15 +0200

    add custom entity extraction snippet

---
+++ EdenAI CSharp Code Snippet/Custom Entity Extraction/CustomEntityExtraction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+public class CustomEntityExtraction
+    {
+    static void Main(string[] args)
+    {
+        HttpClient httpClient = new HttpClient();
+
+
+
+        HttpRequestMessage request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri("https://api.edenai.run/v2/text/custom_named_entity_recognition"),
+            Headers =
+            {
+               { "accept", "application/json" },
+            },
+            Content = new StringContent("{ \"providers\": \"openai\",\"entities\": [\"person\", \"date\"],\"text\":\"I was born in Lyon in February 1996\" ,\"language\" : \"en\"}")
+            {
+                Headers =
+                {
+                  ContentType = new MediaTypeHeaderValue("application/json")
+                }
+            }
+        };
+
+
+        //pass your api key here 
+        request.Headers.Add("Authorization", "Bearer Your_Api_Key_Here");
+
+
+        using (HttpResponseMessage response = httpClient.Send(request))
+        {
+            JObject json = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+            Console.WriteLine((json["openai"]["items"]).ToString());
+
+            if (!response.IsSuccessStatusCode)
+                Console.WriteLine(response.Content);
+        }
+
+        Console.ReadLine();
+
+    }
+
+}
