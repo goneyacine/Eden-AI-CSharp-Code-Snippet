f92adec4e185296f6775035f7eed4992f74b91b3
Yacine Betouche <goneyacine@gmail.com>
2023-03-28 06:49:31 +0200

    add code generation code snippet

---
+++ EdenAI CSharp Code Snippet/Code Generation/CodeGeneration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+
+public class CodeGeneration
+{
+    static void Main(string[] args)
+        {
+        HttpClient httpClient = new HttpClient();
+
+
+        HttpRequestMessage request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Post,
+            RequestUri = new Uri("https://api.edenai.run/v2/text/code_generation"),
+            Headers =
+            {
+               { "accept", "application/json" },
+            },
+            Content = new StringContent("{ \"providers\": \"openai\",\"promot\": \"\",\"instruction\": \"write a code in c++ to calculate distance between to points in 4D space\",\"temprature\" : 0.1,\"max_tokens\":500}")
+            {
+                Headers =
+                {
+                  ContentType = new MediaTypeHeaderValue("application/json")
+                }
+            }
+        };
+
+
+        //pass your api key here 
+        request.Headers.Add("Authorization", "Bearer Your_Api_Key_Here");
+
+
+        using (HttpResponseMessage response = httpClient.Send(request))
+        {
+            JObject json = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+            Console.WriteLine((string)json["openai"]["generated_text"]);
+
+            if (!response.IsSuccessStatusCode)
+                Console.WriteLine(response.Content);
+        }
+
+        Console.ReadLine();
+    }
+    }
